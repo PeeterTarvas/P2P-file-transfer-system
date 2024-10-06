aa49ab60f75a12b370c649bb9e0ce859f760e15b
peeter.tarvas <peetertarvas@gmail.com>
2024-10-06 21:29:44 +0200

    init

---
+++ server/src/main/java/com/server/iot/server/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.server.iot.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Configurer class for configuring cors.
+ */
+@Configuration
+public class CorsConfig  {
+
+    /**
+     * CORS must be processed before Spring Security, because the pre-flight request does not contain any cookies (that is, the JSESSIONID).
+     * If the request does not contain any cookies and Spring Security is first,
+     * the request determines that the user is not authenticated (since there are no cookies in the request) and rejects it.
+     * @return a cors mapping to the front-end side for 4 main http methods.
+     */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true)
+                        .allowedHeaders("*")
+                        .exposedHeaders("*");
+            }
+        };
+    }
+}
---
+++ server/src/main/resources/database.sql
@@ -0,0 +1,19 @@
+CREATE SCHEMA IF NOT EXISTS iot;
+
+CREATE TABLE iot.user (
+         user_id BIGSERIAL PRIMARY KEY,
+         username VARCHAR(255) NOT NULL,
+         password VARCHAR(255) NOT NULL,
+         sys_modified_at TIMESTAMP,
+         sys_deleted_at TIMESTAMP,
+         CONSTRAINT user_name_must_be_unique UNIQUE (username)
+);
+
+CREATE TABLE iot.address (
+      user_id BIGINT NOT NULL,
+      ip VARCHAR(255) NOT NULL,
+      sys_modified_at TIMESTAMP,
+      sys_deleted_at TIMESTAMP,
+      FOREIGN KEY (user_id) REFERENCES iot.user(user_id)  ON UPDATE CASCADE ON DELETE CASCADE,
+      CONSTRAINT user_id_and_ip_must_be_unique UNIQUE (user_id, ip)
+);
